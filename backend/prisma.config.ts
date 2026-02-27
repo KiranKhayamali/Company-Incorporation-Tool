4eb823dc639bd7646899c59085ac9812e4621fb0
Kiran Khayamali <kirankhayamali@gmail.com>
2026-02-27 18:26:58 +0545

    Configuration for migrating database in prisma ORM

---
+++ backend/prisma.config.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrate: {
+    adapter: {
+      provider: "postgresql",
+      url: process.env.DATABASE_URL!,
+    },
+  },
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
